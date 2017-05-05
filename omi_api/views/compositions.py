ef6e988c123d616bc83fa5792f7c5338b2952e19
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2017-05-05 14:46:49 +0200

    Register Composition endpoint

--- omi_api/views/compositions.py
+++ omi_api/views/compositions.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
+from flask_restful import reqparse, Resource, Api
 
 from coalaip import CoalaIp, entities
 from coalaip_bigchaindb.plugin import Plugin
@@ -12,12 +12,48 @@ composition_views = Blueprint('composition_views', __name__)
 composition_api = Api(composition_views)
 
 
-class CompositionApi(Resource):
-    def get(self, entity_id):
-        composition = entities.Work.from_persist_id(
-            entity_id, plugin=coalaip.plugin, force_load=True)
-        return composition.to_jsonld()
+class CompositionListApi(Resource):
+    #def get(self, entity_id):
+    #    composition = entities.Work.from_persist_id(
+    #        entity_id, plugin=coalaip.plugin, force_load=True)
+    #    return composition.to_jsonld()
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        # These are the required parameters
+        parser.add_argument('title', type=str, required=True, location='json')
+        parser.add_argument('composers', type=list, required=True,
+                            location='json')
+        parser.add_argument('songwriters', type=list, required=True,
+                            location='json')
+        parser.add_argument('publishers', type=list, required=True,
+                            location='json')
+        args = parser.parse_args()
+
+        # Here we're transforming from OMI to COALA
+        work = {
+            'name': args['title'],
+            'composers': args['composers'],
+            'songwriters': args['songwriters'],
+            'publishers': args['publishers'],
+        }
+
+        copyright_holder = {
+            "public_key": "Cxj6Pct7T2hLhUh455tbvDWkVDY1vW5aoRGHZCtQkNKQ",
+            "private_key": "8vVgr68Cb5RzUm89nkALvVBmmSoBdC58MTAmqGr8HfYy",
+        }
+
+        work = coalaip.register_work(
+            work_data=work,
+            copyright_holder=copyright_holder
+        )
+
+        work_jsonld = work.to_jsonld()
+        work_jsonld['@id'] = work.persist_id
+
+        return work_jsonld, 200
 
 
-composition_api.add_resource(CompositionApi, '/compositions/<entity_id>',
+composition_api.add_resource(CompositionListApi, '/compositions',
                              strict_slashes=False)
