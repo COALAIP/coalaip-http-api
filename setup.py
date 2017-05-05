c388a5ebe46046364341e3360fbef19f940ce1df
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2017-05-05 16:12:15 +0200

    Remove colorama

--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ from setuptools import setup, find_packages
 install_requires = [
     'coalaip==0.0.1',
     'coalaip-bigchaindb==0.0.1',
-    'bigchaindb~=0.9.1',
     'flask>=0.11.1',
     'flask-restful>=0.3.5',
     'gunicorn>=19.6.0',
     'flask-cors==3.0.2',
-    'colorama~=0.3.0'
 ]
 
 tests_require = [
