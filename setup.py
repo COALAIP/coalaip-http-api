1851d010a8a6b439d7720ca15cff4e2820dde729
vrde <agranzot@gmail.com>
2017-05-06 16:26:32 +0200

    Update setup.py to last vesion of coalaip-bigchaindb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'coalaip==0.0.3',
-    'coalaip-bigchaindb==0.0.3',
+    'coalaip-bigchaindb==0.0.4',
     'flask>=0.11.1',
     'flask-restful>=0.3.5',
     'gunicorn>=19.6.0',
